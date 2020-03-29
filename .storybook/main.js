8feeb22430c3ee2850840291c0171f09e94df9e2
davidhorm <davidhorm+github@gmail.com>
2020-03-29 15:49:42 -0700

    Add Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ['../src/**/*.stories.(ts|tsx|js|jsx)'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-knobs/register',
+  ],
+};
