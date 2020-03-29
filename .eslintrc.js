bb9a8c5ab00716a206a0c00e63a2234f58f2c2b3
davidhorm <davidhorm+github@gmail.com>
2020-03-28 17:22:07 -0700

    Add eslint-plugin-unicorn, config, and --fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,12 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
 
+    'plugin:unicorn/recommended',
+
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
     'prettier/react',
+    'prettier/unicorn',
   ],
   globals: {
     Atomics: 'readonly',
@@ -34,6 +37,8 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
-    'spaced-comment': ['error', 'always', { 'markers': ['/'] }]
+    'spaced-comment': ['error', 'always', { 'markers': ['/'] }],
+    'unicorn/filename-case': 0, // React has their own way of naming things
+    'unicorn/prevent-abbreviations': [ 'error', { 'whitelist': { 'env': true } } ], // Allow react-app-env.d.ts file name
   },
 };
